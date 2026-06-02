d512d9a1efc36c21410c96ebcd9118ba77f0f4a1
Anthony Zheng <108604749+anthony39844@users.noreply.github.com>
2026-06-01 23:39:53 -0500

    Add: valid-tree - submission-7

---
+++ Data Structures & Algorithms/valid-tree/submission-7.py
@@ -0,0 +1,22 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        if len(edges) != n - 1: return False
+
+        graph = defaultdict(list)
+        for x, y in edges:
+            graph[x].append(y)
+            graph[y].append(x)
+        
+        q = deque([(0, None)])
+        visited = set()
+
+        while q:
+            node, parent = q.popleft()
+            visited.add(node)
+
+            for neigh in graph[node]:
+                if node != parent and neigh not in visited:
+                    q.append([neigh, node])
+
+        return len(visited) == n
+        
