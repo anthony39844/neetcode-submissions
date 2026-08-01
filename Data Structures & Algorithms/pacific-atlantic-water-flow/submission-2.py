2201e588123826862ea929eeac83c144951b6b09
Anthony Zheng <108604749+anthony39844@users.noreply.github.com>
2026-08-01 18:50:04 -0500

    Add: pacific-atlantic-water-flow - submission-2

---
+++ Data Structures & Algorithms/pacific-atlantic-water-flow/submission-2.py
@@ -0,0 +1,39 @@
+class Solution:
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        # pacific = row 0 or col 0
+        # atlantic = row (n - 1) or col (n - 1)
+        out = []
+
+        def pac(i, j, visited):
+            if i <= 0 or j <= 0:
+                return True
+            
+            for x, y in ((1, 0), (-1, 0), (0, 1), (0, -1)):
+                a, b = i + x, j + y
+                if 0 <= a < len(heights) and 0 <= b < len(heights[0]) and heights[a][b] <= heights[i][j] and (a, b) not in visited:
+                    visited.add((a, b))
+                    if pac(a, b, visited):
+                        return True
+            
+            return False
+        
+        def atl(i, j, visited):
+            if i >= len(heights) - 1  or j >= len(heights[0]) - 1:
+                return True
+            
+            for x, y in ((1, 0), (-1, 0), (0, 1), (0, -1)):
+                a, b = i + x, j + y
+                if 0 <= a < len(heights) and 0 <= b < len(heights[0]) and heights[a][b] <= heights[i][j] and (a, b) not in visited:
+                    visited.add((a, b))
+                    if atl(a, b, visited):
+                        return True
+
+            return False
+                
+        
+        for i in range(len(heights)):
+            for j in range(len(heights[0])):
+                if pac(i, j, set()) and atl(i, j, set()):
+                    out.append([i, j])
+        
+        return out
