36992819e58277a47051ae8e075e1b2acf1e65a6
Anthony Zheng <108604749+anthony39844@users.noreply.github.com>
2026-08-09 02:14:32 -0500

    Add: search-for-word-ii - submission-10

---
+++ Data Structures & Algorithms/search-for-word-ii/submission-10.py
@@ -0,0 +1,42 @@
+class Solution:
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        
+        trie = {}
+        out = []
+
+        for word in words:
+            cur = trie
+            for i in word:
+                if i not in cur:
+                    cur[i] = {}
+                cur = cur[i]
+            cur["end"] = word
+
+        visited = set()
+        def dfs(cur, i, j):
+            char = board[i][j]
+            level = cur[char]
+            
+            if "end" in level:
+                out.append(level["end"])
+                del level["end"]
+            
+            visited.add((i, j))
+            for x, y in ((1, 0), (-1, 0), (0, 1), (0, -1)):
+                a, b = i + x, j + y
+                if 0 <= a < len(board) and 0 <= b < len(board[0]) and (a, b) not in visited:
+                    c = board[a][b]
+                    if c in level:
+                        dfs(cur[char], a, b)
+            visited.remove((i, j))
+
+            if not level:
+                del cur[char]
+
+        for i in range(len(board)):
+            for j in range(len(board[i])):
+                cur = trie
+                if board[i][j] in cur:
+                    dfs(cur, i, j)
+        
+        return out
