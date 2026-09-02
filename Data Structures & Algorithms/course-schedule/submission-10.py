914faab1fac3232119d5357a315913dd20cc331e
Anthony Zheng <108604749+anthony39844@users.noreply.github.com>
2026-09-02 18:43:33 -0500

    Add: course-schedule - submission-10

---
+++ Data Structures & Algorithms/course-schedule/submission-10.py
@@ -0,0 +1,28 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        adj = defaultdict(list)
+
+        for course, prereq in prerequisites:
+            adj[course].append(prereq)
+        
+        visited = [0] * numCourses
+
+        def dfs(node):
+            if visited[node] == 2:
+                return True
+            if visited[node] == 1:
+                return False
+
+            visited[node] = 1
+            for course in adj[node]:
+                if not dfs(course):
+                    return False
+            visited[node] = 2
+            return True
+                
+
+        for i in range(numCourses):
+            if visited[i] == 0 and not dfs(i):
+                return False
+        
+        return True
